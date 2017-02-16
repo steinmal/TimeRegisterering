f07e7594dfca0aa8950778e1d68e5b9bcb68c844
Steinar Malin <steinar.malin@gmail.com>
2017-02-16 15:04:21 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+include('auth.php');
+?>
+</body>
+</html>
