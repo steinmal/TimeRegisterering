e93815a1eacaecfa888901efe03c7d416e432eb8
steinmal <steinar.malin@gmail.com>
2017-03-15 12:40:32 +0000

    Timegodkjenning påbegynt, bugfixes i brukeradministrering og redigering

--- classes/TeamRegister.class.php
+++ classes/TeamRegister.class.php
@@ -2,6 +2,7 @@
     class TeamRegister {
         private $db;
         
+        
         public function __construct(PDO $db) {
             $this->db = $db;
         }
@@ -13,5 +14,17 @@
             if($team = $stmt->fetchObject('Team')) {
                 return  $team;
             }
+        }
+        
+        public function hentTeamMedlemmer($team_id, $UserReg) {
+            $stmt = $this-db-prepare("SELECT bruker_id FROM teammedlemskap WHERE team_id=:team_id");
+            $stmt->bindparam(':team_id', $team_id, PDO::PARAM_INT);
+            $stmt->execute();
+                
+            $brukere = array();
+            while($bruker_id = $stmt->fetch_assoc('bruker_id')) {
+                $brukere[] = $UserReg.hentBruker($bruker_id);
+            }
+            return $brukere;
         }
     }
