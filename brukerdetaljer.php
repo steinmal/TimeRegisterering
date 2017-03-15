a9a6de8a7781d5fda6d13499b98d70c30b234b17
Ine Haugli <iha117@post.uit.no>
2017-03-15 14:46:02 +0000

    brukerdetaljer, ovesikt prosjekt og team

---
+++ brukerdetaljer.php
@@ -0,0 +1,49 @@
+<?php
+spl_autoload_register(function ($class_name) {
+    require_once 'classes/' . $class_name . '.class.php';
+});
+
+require_once 'vendor/autoload.php';
+include('auth.php');
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+$ProsjektReg = new ProsjektRegister($db);
+$UserReg = new UserRegister($db);
+$TeamReg = new TeamRegister($db);
+
+
+session_start();
+
+if(!isset($_SESSION['innlogget']) || $_SESSION['innlogget'] != true){
+    header("Location: index.php");
+    return;
+}
+
+if(!isset($_SESSION['brukerTilgang'])){
+    echo "Feil";
+    return;
+}
+
+
+$bruker = $_SESSION['bruker'];
+
+$brukerTypeID = $bruker->getBrukerType();
+$brukerType = $UserReg->getBrukerType($brukerTypeID)->getNavn();
+
+$brukerID = $bruker->getBrukerId();
+$teamIDs = $TeamReg->hentTeamIdFraBruker($brukerID);
+var_dump($teamIDs);
+
+$teamliste = array();
+$prosjekter = array();
+foreach ($teamIDs as $i) {
+    $teamliste[] = $TeamReg->hentTeam($teamIDs[$i]);
+    $prosjekter = array_merge($prosjekter, $ProsjektReg->hentProsjekterFraTeam($teamIDs[$i]));
+}
+
+var_dump($teamliste);
+var_dump($prosjekter);
+
+echo $twig->render('brukerdetaljer.html', array('innlogget'=>$_SESSION['innlogget'], 'bruker'=>$bruker, 'prosjekter'=>$prosjekter, 'teamliste'=>$teamliste, 'brukerType'=>$brukerType, 'UserReg'=>$UserReg));
+
+?>
