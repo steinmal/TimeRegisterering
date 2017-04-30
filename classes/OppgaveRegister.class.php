ef52a8505399b794dbf4538fa98f78dc50ff095e
Eirik Stople <est105@post.uit.no>
2017-04-30 20:13:38 +0000

    bugfix oppgavetypetekst

--- classes/OppgaveRegister.class.php
+++ classes/OppgaveRegister.class.php
@@ -48,7 +48,7 @@ class OppgaveRegister {
     // TODO: Duplicate methods, remove one
     public function getOppgavetypeTekst($oppgavetype_id) {
         if ($this->oppgavetyper == null)
-            $this->oppgavetyper = $this->getAlleOppgavetyper();
+            $this->oppgavetyper = $this->hentAlleOppgavetyper();
 
         // TODO: Fiks dette
         if (!isset($this->oppgavetyper[$oppgavetype_id])) {
