cf56eaef3097ec1c71c6467af883626bfc5b6e71
Eirik Stople <est105@post.uit.no>
2017-05-06 12:07:03 +0200

    Database-gjenopprett: porta til mysqli

--- classes/DbHelper.class.php
+++ classes/DbHelper.class.php
@@ -6,69 +6,76 @@
         
         public static function executeMySQLFile($filename) {
 
-//noLimitDumpRestore.php, from http://stackoverflow.com/questions/19751354/how-to-import-sql-file-in-mysql-database-using-php
+            //noLimitDumpRestore.php, from http://stackoverflow.com/questions/19751354/how-to-import-sql-file-in-mysql-database-using-php
 
-// your config
-//$filename = 'yourGigaByteDump.sql';
-$dbHost = 'kark.hin.no';
-$dbUser = 'stud_v17_gruppe2';
-$dbPass = 'gruppe2';
-$dbName = 'stud_v17_gruppe2';
-$maxRuntime = 16; // less then your max script execution limit
+            // your config
+            //$filename = 'yourGigaByteDump.sql';
+            $dbHost = 'kark.hin.no';
+            $dbUser = 'stud_v17_gruppe2';
+            $dbPass = 'gruppe2';
+            $dbName = 'stud_v17_gruppe2';
+            $maxRuntime = 16; // less then your max script execution limit
 
 
-$deadline = time()+$maxRuntime; 
-$progressFilename = $filename.'_filepointer'; // tmp file for progress
-$errorFilename = $filename.'_error'; // tmp file for erro
+            $deadline = time()+$maxRuntime;
+            $progressFilename = $filename.'_filepointer'; // tmp file for progress
+            $errorFilename = $filename.'_error'; // tmp file for erro
 
-mysql_connect($dbHost, $dbUser, $dbPass) OR die('connecting to host: '.$dbHost.' failed: '.mysql_error());
-mysql_select_db($dbName) OR die('select db: '.$dbName.' failed: '.mysql_error());
+            //mysql_connect($dbHost, $dbUser, $dbPass) OR die('connecting to host: '.$dbHost.' failed: '.mysql_error());
+            //mysql_select_db($dbName) OR die('select db: '.$dbName.' failed: '.mysql_error());
 
-($fp = fopen($filename, 'r')) OR die('failed to open file:'.$filename);
+            //$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+			$mysqli = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+            // check connection
+			if ($mysqli->connect_error) {
+				trigger_error('Database connection failed: ' . $mysqli->connect_error, E_USER_ERROR);
+			}
 
-// check for previous error
-//if( file_exists($errorFilename) ){
-//    die('<pre> previous error: '.file_get_contents($errorFilename));
-//}
+            ($fp = fopen($filename, 'r')) OR die('failed to open file:'.$filename);
 
-// activate automatic reload in browser
-//echo '<html><head> <meta http-equiv="refresh" content="'.($maxRuntime+2).'"><pre>';
+            // check for previous error
+            //if( file_exists($errorFilename) ){
+            //    die('<pre> previous error: '.file_get_contents($errorFilename));
+            //}
 
-// go to previous file position
-$filePosition = 0;
-/*if( file_exists($progressFilename) ){
-    $filePosition = file_get_contents($progressFilename);
-    fseek($fp, $filePosition);
-}*/
+            // activate automatic reload in browser
+            //echo '<html><head> <meta http-equiv="refresh" content="'.($maxRuntime+2).'"><pre>';
 
-$queryCount = 0;
-$query = '';
-//while( $deadline>time() AND ($line=fgets($fp, 1024000)) ){
-while($line=fgets($fp, 1024000) ){
-    if(substr($line,0,2)=='--' OR trim($line)=='' ){
-        continue;
-    }
+            // go to previous file position
+            $filePosition = 0;
+            /*if( file_exists($progressFilename) ){
+                $filePosition = file_get_contents($progressFilename);
+                fseek($fp, $filePosition);
+            }*/
 
-    $query .= $line;
-    if( substr(trim($query),-1)==';' ){
-        if( !mysql_query($query) ){
-            $error = 'Error performing query \'<strong>' . $query . '\': ' . mysql_error();
-            file_put_contents($errorFilename, $error."\n");
-            exit;
-        }
-        $query = '';
-        file_put_contents($progressFilename, ftell($fp)); // save the current file position for 
-        $queryCount++;
-    }
-}
+            $queryCount = 0;
+            $query = '';
+            //while( $deadline>time() AND ($line=fgets($fp, 1024000)) ){
+            while($line=fgets($fp, 1024000) ){
+                if(substr($line,0,2)=='--' OR trim($line)=='' ){
+                    continue;
+                }
 
-if( feof($fp) ){
-    //echo 'dump successfully restored!';
-    unlink($progressFilename);
-}else{
-    //echo ftell($fp).'/'.filesize($filename).' '.(round(ftell($fp)/filesize($filename), 2)*100).'%'."\n";
-    //echo $queryCount.' queries processed! please reload or wait for automatic browser refresh!';
-}
+                $query .= $line;
+                if( substr(trim($query),-1)==';' ){
+                    if( !$mysqli->query($query) ){
+                        $error = 'Error performing query \'<strong>' . $query . '\': ' . mysqli_error($mysqli);
+                        file_put_contents($errorFilename, $error."\n");
+                        exit;
+                    }
+                    $query = '';
+                    file_put_contents($progressFilename, ftell($fp)); // save the current file position for
+                    $queryCount++;
+                }
+            }
+
+            if( feof($fp) ){
+                //echo 'dump successfully restored!';
+                unlink($progressFilename);
+            }else{
+                //echo ftell($fp).'/'.filesize($filename).' '.(round(ftell($fp)/filesize($filename), 2)*100).'%'."\n";
+                //echo $queryCount.' queries processed! please reload or wait for automatic browser refresh!';
+            }
         }
     }
 ?>
